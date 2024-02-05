9f27630b9b2b8668b330476df982dd2817d3cc86
Ademar Gomes <me@ademar.uk>
2024-02-05 14:09:31 +0000

    added basic command-line arguments

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-
 #NOTE - Main script to execute the basic file converter
+import argparse
 import os
 from classes.utils import Utils
 from classes.converter import Converter
+
+
+# TODO - define list of extensions based on argument passed
+# TODO - pass the list to listdirectory
+# TODO - make most of the script into functions
+# TODO - use the __main__ piece to avoid it being executed when testing
+# TODO - write unit tests to functions here
+# TODO - work on when no file is found, it is erroring at the moment.
+
+#NOTE - Defines the command line arguments for the script
+def setarguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--directory", help="Converts files in the filder defined: -d <Folder Path>")
+    parser.add_argument("-f","--file", help="Converts the file defined: -f <File Path and Name>")
+    parser.add_argument("-s","--source", help="Source File Type: -s JPEG", choices=['JPEG','PNG','WEBP'],default='JPEG')
+    parser.add_argument("-t","--target", help="Target File Type: -s WEBP", choices=['JPEG','PNG','WEBP'],default='WEBP')
+    return parser.parse_args()
+
+
+args = setarguments()
 
 errors = []
 u = Utils()
@@ -23,3 +43,4 @@ if errors != []:
     for e in errors:
         if e != '':
             print(e)
+
