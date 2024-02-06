86a0ac333cdb4f104f85446b13aa04617b9fdce1
Ademar Gomes <me@ademar.uk>
2024-02-06 18:32:23 +0000

    using extension argument in main

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import argparse
 import os
 from classes.utils import Utils
 from classes.converter import Converter
+from classes.extenions import Extensions
 
 
-# TODO - define list of extensions based on argument passed
-# TODO - pass the list to listdirectory
+# DONE - define list of extensions based on argument passed
+# DONE - pass the list to listdirectory
 # TODO - make most of the script into functions
 # TODO - use the __main__ piece to avoid it being executed when testing
 # TODO - write unit tests to functions here
@@ -22,26 +23,31 @@ def setarguments():
     parser.add_argument("-t","--target", help="Target File Type: -s WEBP", choices=['JPEG','PNG','WEBP'],default='WEBP')
     return parser.parse_args()
 
+if __name__ == '__main__':
 
-args = setarguments()
+    args = setarguments()
 
-errors = []
-utils = Utils()
-converter = Converter()
-files = utils.listdirectory(['.jpeg','.jpg'])
-l = len(files)
+    # set instances
+    utils = Utils()
+    converter = Converter()
+    extensions = Extensions()
 
-# Initial call to print 0% progress
-utils.printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    errors = []
+    lookupext = extensions.getextensions(args.source)
+    files = utils.listdirectory(lookupext)
+    filecount = len(files)
 
-for i,image in enumerate(files):
-    name = image.split('.')[0]+".webp"
-    errors.append(converter.convert(image,name)) 
-    utils.printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    # Initial call to print 0% progress
+    utils.printProgressBar(0, filecount, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-if errors != []:
-    utils.cprint('Errors:','ERROR')
-    for e in errors:
-        if e != '':
-            print(e)
+    for i,image in enumerate(files):
+        name = image.split('.')[0]+".webp"
+        errors.append(converter.convert(image,name))
+        utils.printProgressBar(i + 1, filecount, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    if errors != []:
+        utils.cprint('Errors:','ERROR')
+        for e in errors:
+            if e != '':
+                print(e)
 
